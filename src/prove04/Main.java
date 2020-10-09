20f337b79e3b8b8c110e0c0ced286dcb6b978500
Gui Oba <72475450+guiobaBYUI@users.noreply.github.com>
2020-10-08 19:15:58 -0600

    Responded to the hello.

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -3,6 +3,7 @@ package prove04;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.print("HelloFromAustin");
+        System.out.print("HelloFromAustin");
+        System.out.print("\nHello Austin, my name is Gui.");
     }
 }
